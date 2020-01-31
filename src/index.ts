8c6e3f797f7dae3e551ac03e82f4ce761365caae
Chris Williams <chris.a.williams@gmail.com>
2020-01-31 14:39:28 -0500

    feat: check version references make sense

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
 import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
-import { diff } from "semver"
+import { diff, gte } from "semver"
 import * as fs from "fs-extra"
 import * as path from "path"
 declare var danger: DangerDSLType
@@ -9,16 +9,6 @@ export declare function warn(message: string): void
 export declare function fail(message: string): void
 export declare function markdown(message: string): void
 
-// TODO: What do we want to check?
-// - checks that the module id matches package name?
-// - verify that Jenkinsfile and test/unit/karma.unit.config use an SDK > minSDK of both modules!
-// - flags when multiple platforms have differing versions?
-// - if has js files under assets, the manifest has a "commonjs: true" entry
-// - js file name under assets matches the module id?
-// - module id matches across platforms?
-// - module guid matches across platforms?
-// - platform matches across folder name, manifest, package.json?
-// - "respackage" value for Android?
 interface Manifest {
   name?: string,
 	version?: string,
@@ -31,6 +21,7 @@ interface Manifest {
   architectures?: string,
   apiversion?: string,
   guid?: string,
+  commonjs?: string
 }
 
 function parseManifest(contents: string): Manifest {
@@ -120,6 +111,79 @@ async function exists(relativePath: string, allFiles: string[], rootDir: string)
   return await fs.pathExists(path.join(rootDir, relativePath));
 }
 
+function prepSDKVersion(sdkVersion: string): string {
+  if (sdkVersion) {
+    // strip .GA suffix
+    if (sdkVersion.endsWith('.GA')) {
+      return sdkVersion.slice(0, sdkVersion.length - 3);
+    }
+    // convert .RC suffix to 9.0.0-rc.#
+    if (sdkVersion.endsWith('.RC')) {
+      return sdkVersion.slice(0, sdkVersion.length - 3) + '-rc.1';
+    }
+    // convert .Beta suffix to 9.0.0-beta.1
+    if (sdkVersion.endsWith('.Beta')) {
+      return sdkVersion.slice(0, sdkVersion.length - 5) + '-beta.1';
+    }
+    // handle CI builds with .vTIMESTAMP suffixes!
+    if (sdkVersion.includes('.v')) {
+      return sdkVersion.replace('.v', '-alpha.')
+    }
+  }
+  return sdkVersion
+}
+
+async function checkJenkinsfile(minSDK: string, winningManifest: string, allFiles: string[], rootDir: string): Promise<string | null> {
+  // Check that Jenkinsfile sdkVersion value is > minsdk in manifest!
+  let contents: string;
+  if (allFiles.includes('Jenkinsfile')) {
+    // manifest has changed
+    const diff = await danger.git.diffForFile('Jenkinsfile');
+    contents = diff.after;
+  } else {
+    contents = await fs.readFile(path.join(rootDir, 'Jenkinsfile'), 'utf8');
+  }
+  const matches = contents.match(/sdkVersion\s*=\s*['"]([^'"]+)['"]/);
+  if (matches) {
+    const origSDKVersion = matches[1];
+    const semverCompatVersion = prepSDKVersion(origSDKVersion)
+    if (!gte(semverCompatVersion, `${minSDK}-alpha.0`)) {
+      //  uh-oh, we're trying to build against an sdk that is not >= the stated minSDK
+      fail(`SDK version used to build on Jenkins (${origSDKVersion}) is not >= minSDK of ${minSDK} declared in ${winningManifest}`);
+    }
+    return origSDKVersion;
+  }
+
+  warn(`Was unable to determine SDK version used to build on Jenkins. Consider adding sdkVersion = '${minSDK}' (or greater)`);
+  return null;
+}
+
+async function checkTitaniumXCConfig(relativePath: string, minSDK: string, winningManifest: string, allFiles: string[], rootDir: string): Promise<string | null> {
+  // Check that titanium.xcconfig TITANIUM_SDK_VERSION value is > minsdk in manifest!
+  let contents: string;
+  if (allFiles.includes(relativePath)) {
+    // manifest has changed
+    const diff = await danger.git.diffForFile(relativePath);
+    contents = diff.after;
+  } else {
+    contents = await fs.readFile(path.join(rootDir, relativePath), 'utf8');
+  }
+
+  const matches = contents.match(/TITANIUM_SDK_VERSION\s*=\s*([^\s]+)/);
+  if (matches) {
+    const origSDKVersion = matches[1];
+    const semverCompatVersion = prepSDKVersion(origSDKVersion)
+    if (!gte(semverCompatVersion, `${minSDK}-alpha.0`)) {
+      //  uh-oh, we're trying to build against an sdk that is not >= the stated minSDK
+      fail(`SDK version used to build in XCode (${origSDKVersion} in ${relativePath}) is not >= minSDK of ${minSDK} declared in ${winningManifest}`);
+    }
+    return origSDKVersion;
+  }
+
+  warn(`Was unable to determine SDK version referenced in ${relativePath}. Consider adding TITANIUM_SDK_VERSION = ${minSDK} (or greater)`);
+  return null;
+}
+
 interface LintOptions {
   /**
    * The path to module root
@@ -149,10 +213,49 @@ export default async function lint(options?: LintOptions) {
     }
     // Verify the guid match across platforms
     if (androidManifest.guid !== iosManifest.guid) {
-      fail(`guid is inconsistent across platforms. It is ${androidManifest.guid} in android/manifest and ${iosManifest.guid} in ${iosManifestPath}`);
+      warn(`guid is inconsistent across platforms. It is ${androidManifest.guid} in android/manifest and ${iosManifest.guid} in ${iosManifestPath}`);
     }
     // TODO: warn if versions don't match?
   }
 
-  // TODO: Check that (ios|iphone)/module.xcconfig TITANIUM_SDK_VERSION = 8.1.1.GA values is > minsdk in manifest!
+  // Between the platforms, which has the higher minSDK value?
+  // Note that we expect minSDk values to be vanilla x.y.z version strings
+  const androidMinSDK = androidManifest && androidManifest.minsdk;
+  const iosMinSDK = iosManifest && iosManifest.minsdk;
+  let winningManifest = 'android/manifest'
+  let maxMinSDK = androidMinSDK;
+  if (iosMinSDK) {
+    if (maxMinSDK === undefined || gte(iosMinSDK, maxMinSDK)) {
+      maxMinSDK = iosMinSDK;
+      winningManifest = iosManifestPath;
+    }
+  }
+
+  if (maxMinSDK) {
+    // Verify build/test/ci SDK version is > minSDK and aligns across files!
+    let jenkinsSDKVersion
+    if (await exists('Jenkinsfile', allFiles, moduleRoot)) {
+      jenkinsSDKVersion = await checkJenkinsfile(maxMinSDK, winningManifest, allFiles, moduleRoot);
+    }
+
+    let titaniumXCConfigSDKVersion;
+    if (await exists('ios/titanium.xcconfig', allFiles, moduleRoot)) {
+      titaniumXCConfigSDKVersion = await checkTitaniumXCConfig('ios/titanium.xcconfig', maxMinSDK, winningManifest, allFiles, moduleRoot);
+    } else if (await exists('iphone/titanium.xcconfig', allFiles, moduleRoot)) {
+      titaniumXCConfigSDKVersion = await checkTitaniumXCConfig('iphone/titanium.xcconfig', maxMinSDK, winningManifest, allFiles, moduleRoot);
+    }
+    // TODO: Check that test/unit/karma.unit.config sdkVersion: config.sdkVersion || '9.0.0.v20200130075800' value is > minsdk in manifest!
+
+    // TODO: Check that they all align!
+    if (jenkinsSDKVersion && titaniumXCConfigSDKVersion && jenkinsSDKVersion !== titaniumXCConfigSDKVersion) {
+      warn(`SDK version declared in Jenkinsfile (${jenkinsSDKVersion}) does not match iOS' titanium.xcconfig value (${titaniumXCConfigSDKVersion})`);
+    }
+  }
+
+  // TODO: What do we want to check?
+  // - checks that the module id matches package name?
+  // - if has js files under assets, the manifest has a "commonjs: true" entry
+  // - js file name under assets matches the module id?
+  // - platform matches across folder name, manifest, package.json?
+  // - "respackage" value for Android?
 }
